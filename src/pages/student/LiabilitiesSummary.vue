8b89e70e8a817c58e239c2bef925fcb0baec831a
fencim <fencim@gmail.com>
2025-12-11 09:40:06 +0800

    char

--- src/pages/student/LiabilitiesSummary.vue
+++ src/pages/student/LiabilitiesSummary.vue
@@ -1,10 +1,15 @@
 <template>
   <q-page class="q-pa-lg">
-    <liabilities-section />
+    <liabilities-section
+      :loading="liabilityStore.loading"
+      :liabilities="liabilityStore.myLiabilities"
+    />
   </q-page>
 </template>
 <script setup lang="ts">
 import LiabilitiesSection from 'src/components/LiabilitiesSection.vue';
+import { useLiabilityStore } from 'src/stores/liability-store';
 
+const liabilityStore = useLiabilityStore();
 // Placeholder script
 </script>
